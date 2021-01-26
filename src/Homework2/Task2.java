da3dabde8e44c2c4dd4122a3b24257f7f09b721f
angelinafedchenko <angelinafedchenko>
2021-01-26 17:36:56 +0200

    Homework_2

--- src/Homework2/Task2.java
+++ src/Homework2/Task2.java
@@ -1,8 +1,8 @@
 package Homework2;
 
-        import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task2 {
 
@@ -39,7 +39,7 @@ public class Task2 {
                 System.out.println("Try again!");
         }
 
-System.out.println("Enjoy the time!");
+        System.out.println("Enjoy the time!");
 
     }
 }
